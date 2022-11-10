4358fb722035969ce46c708098a53680d4ded92f
Ulisses <ulissessuporteti01@gmail.com>
2022-11-09 22:38:13 -0300

    criando os cartões a partir do localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css'
+import Form from "./components/Form";
+import { Card } from "./components/Card";
+import GlobalStyle from "./styles/global";
 
 function App() {
-
   return (
     <div className="App">
+      <GlobalStyle />
+      <Form />
+      {Array.from(localStorage).map((item, index) => {
+        const titulo = JSON.parse(
+          localStorage.getItem("card" + index)!.toString()
+        ).titulo;
+        const descricao = JSON.parse(
+          localStorage.getItem("card" + index)!.toString()
+        ).descricao;
+        const status = JSON.parse(
+          localStorage.getItem("card" + index)!.toString()
+        ).status;
+        return (
+          <Card key={"card" + index} titulo={titulo} descricao={descricao} />
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
