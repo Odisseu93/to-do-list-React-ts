e7a1adce34ba62b9e844a445954b65c5758b0edb
Ulisses <ulissessuporteti01@gmail.com>
2022-11-10 23:15:55 -0300

    criado  o styled component ListadeTarefas

---
+++ src/components/Container/ListadeTarefas/ListadeTarefas.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ListadeTarefas = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 90vw;
+  flex-wrap: wrap;
+  font-size: 1.2rem;
+`;
