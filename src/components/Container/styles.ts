2a370ef35b31f8ee5fc57a8a4671c3609b084b76
Ulisses <ulissessuporteti01@gmail.com>
2022-11-10 21:03:36 -0300

    criando o styled component Container

---
+++ src/components/Container/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  width: 90vw;
+  height: 80vh;
+  margin: 1rem auto;
+  border-radius: 20px;
+  background: rgba(39, 245, 101, 0.14);
+  padding: 10px;
+  overflow-y: scroll;
+`;
