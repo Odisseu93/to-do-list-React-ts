72ba9a3efdb4b32da3ffba84b9e680773016c5f9
Ulisses <ulissessuporteti01@gmail.com>
2022-11-10 21:04:20 -0300

    adicionando o Container em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import Form from "./components/Form";
-import { Card } from "./components/Card";
+import { Card } from "./components/Container/Card";
 import GlobalStyle from "./styles/global";
+import { Container } from "./components/Container/styles";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Form />
-      {Array.from(localStorage).map((item, index) => {
-        const titulo = JSON.parse(
-          localStorage.getItem("card" + index)!.toString()
-        ).titulo;
-        const descricao = JSON.parse(
-          localStorage.getItem("card" + index)!.toString()
-        ).descricao;
-        const status = JSON.parse(
-          localStorage.getItem("card" + index)!.toString()
-        ).status;
-        return (
-          <Card key={"card" + index} titulo={titulo} descricao={descricao} />
-        );
-      })}
+      <Container>
+        {Array.from(localStorage).map((item, index) => {
+          const titulo = JSON.parse(
+            localStorage.getItem("card" + index)!.toString()
+          ).titulo;
+          const descricao = JSON.parse(
+            localStorage.getItem("card" + index)!.toString()
+          ).descricao;
+          const status = JSON.parse(
+            localStorage.getItem("card" + index)!.toString()
+          ).status;
+          return (
+            <Card key={"card" + index} titulo={titulo} descricao={descricao} />
+          );
+        })}
+      </Container>
     </div>
   );
 }
