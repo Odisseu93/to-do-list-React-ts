0a1e145f464ccbeda32847c06bbd6bf50330e1e1
Ulisses <ulissessuporteti01@gmail.com>
2022-12-07 18:23:52 -0300

    adicionando o footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  font-size: 20px;
+
+  .wrapper {
+    box-sizing: border-box;
+    padding: 5px;
+    text-align: center;
+    & a {
+      margin-left: 10px;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,24 @@ import { DataContextProvider } from "./contexts/data/contextProvider";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Footer } from "./components/Footer/styles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <DataContextProvider>
       <App />
+      <Footer>
+        <span className="wrapper">
+          Desenvolvido por
+          <a
+            href="https://github.com/Odisseu93"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ulisses Silvério
+          </a>
+        </span>
+      </Footer>
     </DataContextProvider>
   </React.StrictMode>
 );
