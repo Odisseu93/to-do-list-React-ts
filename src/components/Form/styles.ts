19a0c7d15ff19af8e47969218357b8621d8296fd
Ulisses <ulissessuporteti01@gmail.com>
2022-12-07 17:25:15 -0300

    atualizando o estilo do modal

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,29 +1,44 @@
 import styled from "styled-components";
 
-export const Modal = styled.dialog`
+export const Modal = styled.div`
+  display: grid;
+  height: 90vh;
+  width: 99vw;
+  justify-content: center;
   border: none;
+  position: absolute;
   border-radius: 8px;
-  background: #bcead5;
-
-  &::backdrop {
-    background: rgba(11, 11, 11, 0.58);
-  }
+  font-size: 20px;
+  background: rgba(11, 11, 11, 0.58);
 
   form {
+    box-sizing: border-box;
+    width: 360px;
+    height: fit-content;
+    padding: 15px;
+    box-shadow: 0 0 9px blue;
+    font-size: 18px;
+    margin-top: 30px;
+    border-radius: 8px;
+    background: #bcead5;
     display: flex;
     flex-flow: column wrap;
   }
 
   label {
+    font-size: inherit;
     display: block;
     width: 100%;
     text-align: center;
   }
 
   input[type="text"] {
+    font-size: inherit;
     width: 100%;
   }
   textarea {
+    width: 325px;
+    font-size: inherit;
     display: block;
     border: none;
   }
@@ -45,6 +60,7 @@ export const Modal = styled.dialog`
   }
 
   .status {
+    font-size: inherit;
     margin: 10px 0;
     display: flex;
   }
@@ -52,8 +68,10 @@ export const Modal = styled.dialog`
   b {
     width: min-content;
     margin-right: 3px;
+    font-size: 20px;
   }
   select {
     width: fit-content;
+    font-size: inherit;
   }
 `;
